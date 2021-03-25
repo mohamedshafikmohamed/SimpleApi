7a46ba9e7240fa5421bf976a1299e9e9dd945e04
islam <islam@DESKTOP-UDNN17H>
2021-03-25 16:55:00 +0200

    Add project files.

---
+++ api2/Migrations/20210324165437_first.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api2.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "products");
+        }
+    }
+}
---
+++ api2/models/ProductRepos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api2.models
+{
+  public  interface ProductRepos
+    {
+       public IEnumerable<product> GetProducts();
+        public IEnumerable<product> GetProductsByName(string name);
+       public  product GetProduct(int id); 
+        public  void Delete(int id);
+        public  void create(product p);
+        public  void update(product p);
+    }
+}
